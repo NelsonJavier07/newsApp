c0690e609aa7b7567ffa5664cb817789802e873a
Nelson <nelson070585@gmail.com>
2024-01-02 08:01:34 -0300

    Solucionado el problema con renderizado(problema con atributo style en etiqueta)

--- src/Components/NewsCard/NewsCard.jsx
+++ src/Components/NewsCard/NewsCard.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import '../NewsCard/NewsCard.css';
 
 export const NewsCard = ({noti}) => {
 
@@ -7,13 +8,13 @@ export const NewsCard = ({noti}) => {
     const imgNewsPath = `/assets/${noti.author}.svg`;
 
     return (
-        <div className="card" style="width: 18rem;">
+        <div className="card cardCssLocal">
             <img src={ imgNewsPath } className="card-img-top" alt="" />
                 <div className="card-body">
                     <h4 className="card-title">Titulo: { noti.title }</h4>
                     <p className="card-text"> Fuente:{ noti.author }</p>
                     <small className='card-text'>Fecha de publicación: {noti.publishedAt}</small>
-                    <Link to={ noti.url } className="btn btn-primary">Ir a la noticia</Link>
+                    <Link to={ noti.url } className="btn btn-primary" target='blank'>Ir a la noticia</Link>
                 </div>
         </div>
     )
