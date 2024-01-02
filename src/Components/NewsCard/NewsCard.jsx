6bf34a6ad05d3c94f63f1c64defa0f812f7be0fc
Nelson <nelson070585@gmail.com>
2024-01-02 17:35:39 -0300

    Correcciones y renderizacion de imagenes ilustrativas

--- src/Components/NewsCard/NewsCard.jsx
+++ src/Components/NewsCard/NewsCard.jsx
@@ -5,14 +5,16 @@ export const NewsCard = ({noti}) => {
 
     
 
-    const imgNewsPath = `/assets/${noti.author}.svg`;
+    const imgNewsPath = `src/assets/${noti.author}.svg`;
+
+    console.log(imgNewsPath);
 
     return (
         <div className="card cardCssLocal">
             <img src={ imgNewsPath } className="card-img-top" alt="" />
                 <div className="card-body">
-                    <h4 className="card-title">Titulo: { noti.title }</h4>
-                    <p className="card-text"> Fuente:{ noti.author }</p>
+                    <h4 className="card-title">{ noti.title }</h4>
+                    <p className="card-text"> Fuente: { noti.author }</p>
                     <small className='card-text'>Fecha de publicación: {noti.publishedAt}</small>
                     <Link to={ noti.url } className="btn btn-primary" target='blank'>Ir a la noticia</Link>
                 </div>
--- src/Components/NewsGrid/NewsGrid.jsx
+++ src/Components/NewsGrid/NewsGrid.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-// import { getNews } from '../../../utils/httpClient.js';
 import axios from 'axios';
 
 import '../NewsGrid/NewsGrid.css';
@@ -11,10 +10,7 @@ export const NewsGrid = () => {
 
     
     const url = 'https://newsapi.org/v2/top-headlines?country=ar&apiKey=34bb8890a09f4be9be849b19707857f3'
-    // const url = 'https://newsapi.org/v2/';
-    // const urlPath = 'top-headlines?' +
-    //     'country=ar&' +
-    //     'apiKey=34bb8890a09f4be9be849b19707857f3';
+    
     
         const [news, setNews] = useState ([]);
         
@@ -22,26 +18,24 @@ export const NewsGrid = () => {
 
     useEffect(() => {
 
-    // const infoNews = () => {
+    
         axios.get(url)
         .then((res) => {setNews(res.data.articles);
         console.log(res.data.articles);
         })
         .catch((error) => console.log(error))
-    // }
+    
 
     }, [])
 
     
 
     return(
-        <ul className='notiContainer list-group'>
-            <li className='list-group-item'>
+            <div className='listContainer'>
                 {
-                    news.map((noti) => { return <NewsCard key={uuidv4()} noti={noti}  />})
+                    news.map((noti) => { return <NewsCard key={uuidv4()} noti={noti} />})
                 }
-            </li>
-        </ul>
+            </div>
     )
 }
 
